e2f351c89b9100fcc0c26ba8cecdd82f71e49f21
alexey shirshov <alex.shirshov@gmail.com>
2020-10-10 14:02:28 +0300

    Reorder files and and boolean render filter

---
+++ WebEx.Core/RenderFilters/BoolRenderFilter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace WebEx.Core
+{
+    public class BoolRenderFilter : IPreRenderFilter
+    {
+        private readonly bool _condition;
+
+        public BoolRenderFilter(bool condition)
+        {
+            _condition = condition;
+        }
+        public PreRenderFilterResult Exec(HtmlHelper helper, string moduleInstanceId, IDictionary<string, object> args, string renderedViewName, IModuleView view)
+        {
+            if (!_condition)
+                return PreRenderFilterResult.DontRender;
+
+            return null;
+        }
+    }
+}
